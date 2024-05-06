58dd470d96ada29af12ce940d0303227d7378a81
Rej327 <resujeff27@gmail.com>
2024-05-06 18:23:41 +0800

    Add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Main from "./view/Main";
 import Mobile from "./view/Mobile";
+import { okcupid } from "./assets";
 
 function App() {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setLoading(false);
+		}, 2000);
+
+		return () => clearTimeout(timeout);
+	}, []);
+
 	return (
-		<>
-			<Main />
-			<Mobile />
-		</>
+		<div className="App">
+			{loading ? (
+				<div className="loader">
+					<img src={okcupid} alt="loading" />
+				</div>
+			) : (
+				<>
+					<Main />
+					<Mobile />
+				</>
+			)}
+		</div>
 	);
 }
 
